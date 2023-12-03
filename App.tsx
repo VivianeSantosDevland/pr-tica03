19ae9bde31358a7d804803a4781012ea8f98eeb3
VivianeSantosDevland <vivianesantosdevland@gmail.com>
2023-12-03 08:26:42 -0300

    Agora é só brincar de react-native...

--- App.tsx
+++ App.tsx
@@ -37,45 +37,4 @@ viewBtn:{
 })
 
 
-
-
 export default (App);
-/*
-<LayoutModalD></LayoutModalD>
-<LayoutModalD title="Este é um modal"></LayoutModalD>
-
-<LayoutSplitterD_ orientacao={false}>
-        <BoxSplitterD tamanhoBox={0.2} background="#410751">
-          <Text style={{color: "#ffffff", fontSize:20, margin: "auto"}}>Este é o box ummmmmmmm</Text>
-        </BoxSplitterD>
-        <BoxSplitterD tamanhoBox={0.8} background="#26003A">
-          <Text style={{color: "#ffffff", fontSize:20, margin: "auto"}}>Este é o box doiiiiiiiiiiiissssssssss</Text>
-        </BoxSplitterD>
-      </LayoutSplitterD_>
-
-
-
-
-
-
-<LayoutSplitterD tamanhoBox1={0.2} tamanhoBox2={0.8} orientacao={true}>
-        
-      </LayoutSplitterD>
-//<LayoutSplitter></LayoutSplitter>
-//<LayoutModal></LayoutModal>
-//<Buttonbtn>Click here!</Buttonbtn>
-//<Label>Hello world!</Label>
-//<TextEdit/>
-//<LayoutModalD title="Título aqui">
- // <Text>Testando modal</Text>
- // </LayoutModalD>
- //  <ButtonD title="Clique" pressOn={beta}></ButtonD>
-//<TextEditD textLabel="Nome:"></TextEditD>
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});*/
